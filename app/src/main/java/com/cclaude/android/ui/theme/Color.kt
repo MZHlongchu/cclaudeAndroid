3b0a6faada58787a616322ee1010f5644c11dc39
navy <2603451187@qq.com>
2026-03-19 14:19:17 +0800

    Add file

---
+++ app/src/main/java/com/cclaude/android/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.cclaude.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary colors
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom colors for CClaude
+val ClaudeOrange = Color(0xFFE57035)
+val ClaudeOrangeLight = Color(0xFFF5A623)
+val ClaudeBackground = Color(0xFFFAFAFA)
+val ClaudeSurface = Color(0xFFFFFFFF)
+val ClaudeText = Color(0xFF1F1F1F)
+val ClaudeTextSecondary = Color(0xFF5F5F5F)
+val ClaudeBorder = Color(0xFFE5E5E5)
+val ClaudeUserBubble = Color(0xFFE57035)
+val ClaudeAssistantBubble = Color(0xFFF5F5F5)
+val ClaudeError = Color(0xFFE53935)
+val ClaudeSuccess = Color(0xFF43A047)
